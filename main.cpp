6e80e28f3fe0de457f425659a1156fb8bc78d36b
lexxy91 <rajiolalekan29@yahoo.com>
2017-12-08 00:05:11 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int raji[10];
+        int *rp0 = &raji[0];
+        int *rp1 = &raji[1];
+
+        cout << "rp0 is at" << rp0 << endl;
+        cout << "rp1  is at" << rp1 << endl;
+}
+
